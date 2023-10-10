6301e49889fc89b5fa39ed2a64f70d374cb685fb
linhhatay <linhhatay2404@gmail.com>
2023-10-10 16:00:35 +0700

    backend error handling and custom error

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,16 @@
+const AppError = require("../utils/appError");
 const userRouter = require("./userRoutes");
+const globalErrorHandler = require("../controllers/errorController");
 
 function route(app) {
   app.use("/api/v1/users", userRouter);
+
+  app.all("*", (req, res, next) => {
+    next(
+      new AppError(`Can't not  find ${req.originalUrl} on this server!`, 404)
+    );
+  });
+  app.use(globalErrorHandler);
 }
 
 module.exports = route;
